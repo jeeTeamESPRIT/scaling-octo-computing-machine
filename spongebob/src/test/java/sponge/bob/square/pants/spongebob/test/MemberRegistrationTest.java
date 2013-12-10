2a2b10b9f469d7099cb11e478f7c44ba7d7365ea
wael.aniba <wael.aniba@192.168.1.100>
2013-12-10 11:49:05 +0100

    Signed-off-by: wael.aniba <wael.aniba@192.168.1.100>

--- spongebob/src/test/java/sponge/bob/square/pants/spongebob/test/MemberRegistrationTest.java
+++ spongebob/src/test/java/sponge/bob/square/pants/spongebob/test/MemberRegistrationTest.java
@@ -16,56 +16,16 @@
  */
 package sponge.bob.square.pants.spongebob.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import sponge.bob.square.pants.spongebob.model.Member;
-import sponge.bob.square.pants.spongebob.service.MemberRegistration;
-import sponge.bob.square.pants.spongebob.util.Resources;
 
-@RunWith(Arquillian.class)
 public class MemberRegistrationTest {
-    @Deployment
-    public static Archive<?> createTestArchive() {
-        return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addClasses(Member.class, MemberRegistration.class, Resources.class)
-                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                // Deploy our test datasource
-                .addAsWebInfResource("test-ds.xml");
-    }
-
-    @Inject
-    MemberRegistration memberRegistration;
-
-    @Inject
-    Logger log;
-
+   
     @Test
     public void testRegister() throws Exception {
-        Member newMember = new Member();
-        newMember.setName("Jane Doe");
-        newMember.setEmail("jane@mailinator.com");
-        newMember.setPhoneNumber("2125551234");
-        memberRegistration.register(newMember);
-        newMember.setId(null);
         fail("N'importe quoi!");
-        log.info(newMember.getName() + " was persisted with id " + newMember.getId());
-        
-        
-        
     }
 
 }
